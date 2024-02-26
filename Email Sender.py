aee3adf7edbe102ac1f05ee0e7b47b1b37e56140
Mahad-byte <113996965+Mahad-byte@users.noreply.github.com>
2024-02-26 16:49:26 +0500

    Update Email Sender.py
    Correct code for sending gmail with Title and body

--- Email Sender.py
+++ Email Sender.py
@@ -1,35 +1,32 @@
-import email.mime.text as body
-import email.mime.multipart as multipart
-message = multipart()
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-class SendEmail: 
 
-    def __init__(self, senderEmail, receiverEmail, subject):
-        message["From"] = senderEmail
-        message["To"] = receiverEmail
-        message["subject"] = subject
-        
+class SendEmail:
+
+    def __init__(self, sender_email, receiver_email, subject):
+        self.message = MIMEMultipart()
+        self.message["From"] = sender_email
+        self.message["To"] = receiver_email
+        self.message["Subject"] = subject
+
     def secure_connection(self):
         import smtplib
         s = smtplib.SMTP('smtp.gmail.com', 587)
         s.starttls()
         s.login(user='mahadnaeem416@gmail.com', password='rguuwaeyqpcvwkup')
-        s.sendmail(from_addr='mahadnaeem416@gmail.com', to_addrs='mahadnaeem416@gmail.com', msg=str(self.myOTP))
-
-    def MessageBody(self, text):
-        message.attach(body, text)
-        
-    def quit_connection(self, s):
-        import smtplib
-        s = smtplib.SMTP('smtp.gmail.com', 587)
+        s.sendmail(from_addr='mahadnaeem416@gmail.com', to_addrs='mahadnaeem416@gmail.com',
+                   msg=self.message.as_string())
         s.quit()
+
+    def message_body(self, text):
+        self.message.attach(MIMEText(text))
 
 
 def __main__():
     p = SendEmail('mahadnaeem416@gmail.com', 'mahadnaeem416@gmail.com', 'First Email')
+    p.message_body("Hello How are you!!!")
     p.secure_connection()
-    p.MessageBody('Hello how are you!!!')
-    p.quit_connection(p)
 
 
 if __name__ == '__main__':
