67af71cbea2b3da6cb804490a4631ef7201784aa
Mahad-byte <113996965+Mahad-byte@users.noreply.github.com>
2024-03-11 16:12:26 +0500

    Update Email Sender.py
    Added list email to target multiple users

--- Email Sender.py
+++ Email Sender.py
@@ -31,3 +31,47 @@ def __main__():
 
 if __name__ == '__main__':
     __main__()
+
+
+
+
+
+
+
+
+
+# from email.mime.text import MIMEText
+# from email.mime.multipart import MIMEMultipart
+
+
+# class SendEmail:
+
+#     def __init__(self, sender_email, receiver_email, subject):
+#         self.message = MIMEMultipart()
+#         self.message["From"] = sender_email
+#         self.message["To"] = receiver_email
+#         self.message["Subject"] = subject
+
+#     def secure_connection(self):
+#         import smtplib
+#         s = smtplib.SMTP('smtp.gmail.com', 587)
+#         s.starttls()
+#         s.login(user='mahadnaeem416@gmail.com', password='rguuwaeyqpcvwkup')
+#         s.sendmail(from_addr='mahadnaeem416@gmail.com', to_addrs=email,
+#                    msg=self.message.as_string())
+#         s.quit()
+
+#     def message_body(self, text):
+#         self.message.attach(MIMEText(text))
+
+
+# def __main__():
+#     for x in email:
+#         p = SendEmail('mahadnaeem416@gmail.com', x, 'First Email')
+#         p.message_body("Hello How are you!!!")
+#         p.secure_connection()
+
+
+# if __name__ == '__main__':
+#     email = ["mahadnaeem416@gmail.com'", "hammadzulfiqar555@gmail.com"]
+#     __main__()
